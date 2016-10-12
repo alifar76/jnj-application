22c54cf19286a35efe0aa222a0633fd12e849464
Ali Ahmad Faruqi <alifaruqi@Alis-MacBook-Pro.local>
2016-10-12 10:14:22 -0700

    changed Task C to Hall strain

--- code/task_c.py
+++ code/task_c.py
@@ -9,31 +9,34 @@ Published: Jan 2014
 Strain # 1: Clostridium botulinum A str. ATCC 3502
 NCBI: https://www.ncbi.nlm.nih.gov/nuccore/NC_009495?report=genbank
 
-Strain # 2: Clostridium botulinum A str. ATCC 19397
-NCBI: https://www.ncbi.nlm.nih.gov/nuccore/NC_009697?report=genbank
+Strain # 2: Clostridium botulinum A str. Hall chromosome
+NCBI: https://www.ncbi.nlm.nih.gov/nuccore/NC_009698
 """
 
-import zipfile, os, subprocess
+import os, subprocess
 from Bio import Entrez
 
-
-TERM = "NC_009697 OR NC_009495"
 MAUVE_LOC = '/Applications/Mauve.app/Contents/MacOS/progressiveMauve'
 Entrez.email = "A.N.Other@example.com"
+strain_dict = {'NC_009495':'ATCC_3502','NC_009698':'Hall_A'} 
+#TERM = "NC_009697 OR NC_009495"
+
+accession = strain_dict.keys()
+gid_name = {}
+
+for x in accession:
+	handle = Entrez.esearch(db="nuccore", term=x)
+	record = Entrez.read(handle)
+	gid_name[record["IdList"][0]] = x
 
 
-handle = Entrez.esearch(db="nuccore", term=TERM)
-record = Entrez.read(handle)
-gi_list = record["IdList"]
-
-
-for x in gi_list:
+for x in gid_name.keys():
 	handle2 = Entrez.efetch(db="nuccore", id=x, rettype="fasta", retmode="text")
-	outfile = open(x+".fasta","w")
+	outfile = open(strain_dict[gid_name[x]]+".fasta","w")
 	outfile.write(handle2.read())
 	outfile.close()
 
 runmauve = '{0} --output=clostridia.xmfa {1} \
-{2}'.format(MAUVE_LOC,gi_list[0]+".fasta",gi_list[1]+".fasta")
-
+{2}'.format(MAUVE_LOC,strain_dict.values()[0]+".fasta",strain_dict.values()[1]+".fasta")
 subprocess.call(runmauve, shell=True)
+
